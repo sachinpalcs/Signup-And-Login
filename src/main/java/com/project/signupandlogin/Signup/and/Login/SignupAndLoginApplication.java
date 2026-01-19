4e117b5cc75f8139bb27a0bf8ce3c34556982d5e
sachinpalcs <sachin004pal@gmail.com>
2026-01-19 09:52:01 +0530

    initial commit

---
+++ src/main/java/com/project/signupandlogin/Signup/and/Login/SignupAndLoginApplication.java
@@ -0,0 +1,13 @@
+package com.project.signupandlogin.Signup.and.Login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SignupAndLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SignupAndLoginApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/signupandlogin/Signup/and/Login/SignupAndLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.signupandlogin.Signup.and.Login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SignupAndLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
